4e4a8992695a3fd43d262091dae08d03fc8e13b4
Sunny Radadiya <radadiyasunny970@gmail.com>
2021-03-16 21:14:12 +0530

    Minor bug fix for biconomy provider

--- src/biconomy/biconomy.ts
+++ src/biconomy/biconomy.ts
@@ -1,25 +1,40 @@
 import { Biconomy } from '@biconomy/mexa'
 import { BICONOMY_API_KEY } from "../constants/config";
 
-const biconomy = new Biconomy(window.ethereum, { apiKey: BICONOMY_API_KEY })
 let ercForwarderClient: any
 let permitClient: any
-
-biconomy
-  .onEvent(biconomy.READY, () => {
-    ercForwarderClient = biconomy.erc20ForwarderClient
-    permitClient = biconomy.permitClient
-  })
-  .onEvent(biconomy.ERROR, () => {})
+let biconomy: any
+if(window && window.ethereum) {
+  biconomy = new Biconomy(window.ethereum, { apiKey: BICONOMY_API_KEY })
+  
+  biconomy
+    .onEvent(biconomy.READY, () => {
+      ercForwarderClient = biconomy.erc20ForwarderClient
+      permitClient = biconomy.permitClient
+    })
+    .onEvent(biconomy.ERROR, () => {})
+}
 
 export function getErcForwarderClient(): any {
+  try {
     return ercForwarderClient
+  } catch (error) {
+    console.log('ERCForwarderClient-error', error)
+  }
 }
 
 export function getPermitClient(): any {
+  try {
     return permitClient
+  } catch (error) {
+    console.log('PermitClient-error', error)
+  }
 }
 
 export function getBiconomy(): any {
+  try {
     return biconomy
+  } catch (error) {
+    console.log('BiconomyProvider-error', error)
+  }
 }
