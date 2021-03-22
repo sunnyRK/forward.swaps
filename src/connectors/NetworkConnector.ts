c34137c1b5ae8190a104374a444eecb9b7948998
livingrock7 <chiragtitiya@exvillage.com>
2021-03-23 03:28:18 +0530

    fix network connector related issue ii

--- src/connectors/NetworkConnector.ts
+++ src/connectors/NetworkConnector.ts
@@ -47,7 +47,7 @@ class MiniRpcProvider implements AsyncSendable {
     this.host = parsed.host
     this.path = parsed.pathname
     // how long to wait to batch calls
-    this.batchWaitTimeMs = batchWaitTimeMs ?? 50
+    this.batchWaitTimeMs = batchWaitTimeMs ?? 1000
   }
 
   public readonly clearBatch = async () => {
@@ -63,7 +63,8 @@ class MiniRpcProvider implements AsyncSendable {
         body: JSON.stringify(batch.map(item => item.request))
       })
     } catch (error) {
-      batch.forEach(({ reject }) => reject(new Error('Failed to send batch call')))
+      //batch.forEach(({ reject }) => reject(new Error('Failed to send batch call')))
+      console.log(error)
       return
     }
 
